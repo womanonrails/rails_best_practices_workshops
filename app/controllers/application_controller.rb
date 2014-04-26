3080eeaef718af953a1f9673fe2870f3dd4607fc
Agnieszka Matysek <amatysek@fractalsoft.org>
2014-04-26 17:38:02 +0200

    Added name to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  def configure_permitted_parameters
+    sign_up_sanitizer << :name
+    account_update_sanitizer << :name
+  end
+
+  def sign_up_sanitizer
+    @sign_up_sanitizer ||= devise_parameter_sanitizer.for(:sign_up)
+  end
+
+  def account_update_sanitizer
+    @account_update_sanitizer ||= devise_parameter_sanitizer.for(:account_update)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426090913) do
+ActiveRecord::Schema.define(version: 20140426131017) do
 
   create_table "offers", force: true do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140426090913) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
